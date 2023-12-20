5ada6047ecdb993a17fb89e030ade0ebecaf49a1
Aiko Ichikura <ichikura@jsk.imi.i.u-tokyo.ac.jp>
2023-12-20 15:08:41 +0900

    add files for moview shooting

---
+++ src/kuromitsu/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+import rospy
+import time
+from std_msgs.msg import String
+from std_msgs.msg import UInt16
+from std_msgs.msg import ColorRGBA
+
+import situation_1
+
+if __name__ == "__main__":
+    situation_1.scene_1()
---
+++ src/kuromitsu/situation_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+import rospy
+import time
+from std_msgs.msg import String
+from std_msgs.msg import UInt16
+from std_msgs.msg import ColorRGBA
+import module 
+#from robotmodel import *
+
+scene_node = module.Scene_node()
+
+def eye(mode):
+    scene_node.eye(mode)
+
+def cheek_led(r,g,b,brightness=10,mode=1,blink=3,duration=1,rainbow_hue=1):
+    scene_node.cheek_led(r,g,b,brightness,mode,blink,duration,rainbow_hue)
+
+def ume_led(r,g,b,brightness=10,mode=1,blink=3,duration=1,rainbow_hue=1):
+    scene_node.ume_led(r,g,b,brightness,mode,blink,duration,rainbow_hue)
+
+#############################################
+#######　編集するのはこの下から！ ###########
+#############################################
+
+
+def scene_1():
+    time.sleep(2)
+    eye(6)
+    cheek_led(r=255,g=105,b=180,mode=1)
+    time.sleep(3)
+    eye(5)
+    cheek_led(r=135,g=206,b=235,brightness=10,mode=1)
+    time.sleep(3)
+    eye(2)
+    cheek_led(r=255,g=255,b=255,brightness=30,mode=3)
+    #print("{} done".format("cheek_led"))
+    #act("init") #動作の名前を入力
+    
